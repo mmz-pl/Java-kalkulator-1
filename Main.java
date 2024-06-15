8962bd56ad47a39da5194bdc14aec2151d8ff639
mmz-pl <mateusz@qsgroup.eu>
2024-06-15 14:25:20 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,43 +2,66 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-        public class Main{
-public static void main(String[]args){
+public class Main {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Kalkulator 2 wartości");
-    try{
-        System.out.println("Podaj pierwszą wartość");
-            double a = scanner.nextDouble();
-        System.out.println("Podaj znak operacji arytmetycznej: +,-,*,/ ");
-            String x = scanner.next();
-            char znak = x.charAt(0);
-        System.out.println("Podaj drugą wartość");
-            double b = scanner.nextDouble();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kalkulator 2 wartości");
 
-        if (znak == '+'){
-            System.out.println("Wynik tego działania to: " + (a+b));
-        }else if (znak == '-'){
-            System.out.println("Wynik tego działania to: " + (a-b));
-        }else if (znak == '*'){
-            System.out.println("Wynik tego działania to: " + (a*b));
-        }else if(znak == '/'){
-            if(b==0){
-                System.out.println("Nie można dzielić przez 0!");
-            }else{
-                System.out.println("Wynik tego działania to: " + (a/b));
+
+        double a=0;
+        double b=0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.println("Podaj pierwszą wartość");
+                a = scanner.nextDouble();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Wprowadzono niepoprawną wartość! Spróbuj ponownie.");
+                scanner.next();
             }
-        }else{
+        }validInput = false;
+
+
+        System.out.println("Podaj znak operacji arytmetycznej: +,-,*,/ ");
+        String x = scanner.next();
+        char znak = x.charAt(0);
+
+
+
+        while (!validInput) {
+            try {
+                System.out.println("Podaj drugą wartość");
+                b = scanner.nextDouble();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Wprowadzono niepoprawną wartość! Spróbuj ponownie.");
+                scanner.next();
+            }
+        }validInput = false;
+
+
+
+        if (znak == '+') {
+            System.out.println("Wynik tego działania to: " + (a + b));
+        } else if (znak == '-') {
+            System.out.println("Wynik tego działania to: " + (a - b));
+        } else if (znak == '*') {
+            System.out.println("Wynik tego działania to: " + (a * b));
+        } else if (znak == '/') {
+
+            if (b == 0) {
+                System.out.println("Nie można dzielić przez 0!");
+            } else {
+                System.out.println("Wynik tego działania to: " + (a / b));
+            }
+        } else {
             System.out.println("Wprowadzono niepoprawny znak arytmetyczny!");
         }
-    }catch (InputMismatchException e){
-        System.out.println("Wprowadzono niepoprawną wartość! Spróbuj ponownie.");
-    }finally {
-        scanner.nextLine();
-        System.out.println("Naciśnij Enter aby zakończyć program!");
-        scanner.nextLine();
-        scanner.close();
+
+
     }
-    System.out.println("Program zakończył działanie!");
-}
+
 }
